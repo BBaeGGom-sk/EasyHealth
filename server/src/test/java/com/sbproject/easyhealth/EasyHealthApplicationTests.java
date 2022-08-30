e143442afd0e5b11bb2296ac3eaf78190f36c080
BBaeGGom <dhtmdrl2004@naver.com>
2022-08-31 07:05:45 +0900

    Initial commit

---
+++ server/src/test/java/com/sbproject/easyhealth/EasyHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbproject.easyhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
